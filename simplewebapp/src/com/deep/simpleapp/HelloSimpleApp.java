2f09ca6c75382a1c022d42739639f29320a96b75
DeepikaM <DeepikaM@quinnox.com>
2018-06-06 11:12:50 +0530

    new comment.

--- simplewebapp/src/com/deep/simpleapp/HelloSimpleApp.java
+++ simplewebapp/src/com/deep/simpleapp/HelloSimpleApp.java
@@ -3,5 +3,6 @@ package com.deep.simpleapp;
 public class HelloSimpleApp {
 public static void main(String[] args) {
 	System.out.println("Hello Simple App");
+	System.out.println("Welcome GitHub");
 }
 }
